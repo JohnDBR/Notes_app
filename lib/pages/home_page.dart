cc8268c7f99ed18c935d5c126e6e50ccf5fcd49c
John Barbosa <djbarbosa@uninorte.eud.co>
2020-04-14 05:53:40 -0500

    Dismissable widdget added

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -7,7 +7,8 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  List<Note> notes = new List<Note>();
+  //List<Note> notes = new List<Note>();
+  List<String> notes = List<String>.generate(20, (i) => "Item ${i + 1}");
 
   @override
   Widget build(BuildContext context) {
@@ -23,7 +24,7 @@ class _HomePageState extends State<HomePage> {
   }
 
   Widget _list(){
-      return ListView.builder(
+    return ListView.builder(
       itemCount: notes.length,
       itemBuilder: (context, position) {
         var element = notes[position];
@@ -32,13 +33,91 @@ class _HomePageState extends State<HomePage> {
     );
   }
 
-  Widget _item(Note element, int position){
-    return Text('$position');
+  Widget _item(String element, int position){
+    return Dismissible(
+      background: _backgroundSlide(),
+      key: UniqueKey(),
+      
+      confirmDismiss: (direction) async {
+        if (direction == DismissDirection.startToEnd) {
+          final bool res = await showDialog(
+              context: context,
+              builder: (BuildContext context) {
+                return AlertDialog(
+                  content: Text(
+                      "Are you sure you want to delete ${notes[position]}?"),
+                  actions: <Widget>[
+                    FlatButton(
+                      child: Text(
+                        "Cancel",
+                        style: TextStyle(color: Colors.black),
+                      ),
+                      onPressed: () {
+                        Navigator.of(context).pop();
+                      },
+                    ),
+                    FlatButton(
+                      child: Text(
+                        "Delete",
+                        style: TextStyle(color: Colors.red),
+                      ),
+                      onPressed: () {
+                        setState(() {
+                          notes.removeAt(position);
+                        });
+                        Navigator.of(context).pop();
+                      },
+                    ),
+                  ],
+                );
+              });
+          return res;
+        } else {
+          // TODO: Navigate to edit page;
+        }
+      },      
+      
+      child: InkWell(
+          onTap: () {
+            print("${notes[position]} clicked");
+          },
+          child: ListTile(title: Text('${notes[position]}'))
+        ) // ListTile(title: Text('$element')),
+    );
+  }
+
+  Widget _backgroundSlide(){
+    return Container(
+      color: Colors.red,
+      child: Align(
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: <Widget>[
+            Icon(
+              Icons.delete,
+              color: Colors.white,
+            ),
+            Text(
+              " Delete",
+              style: TextStyle(
+                color: Colors.white,
+                fontWeight: FontWeight.w700,
+              ),
+              textAlign: TextAlign.left,
+            ),
+            SizedBox(
+              width: 20,
+            ),
+          ],
+        ),
+        alignment: Alignment.centerRight,
+      )
+    );
   }
 
   void _addNote(){
     setState(() {
-      notes.add(new Note(title: "itemT", body: "itemB", completed: 0 ));
+      //notes.add(new Note(title: "itemT", body: "itemB", completed: 0 ));
     });
   }
 }
