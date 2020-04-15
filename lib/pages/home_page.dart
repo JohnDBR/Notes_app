79d5d37efc1412cf65c160ab57ebd9746708b888
nicolasmorales1 <nicolasmorales@uninorte.edu.co>
2020-04-14 19:14:08 -0500

    card

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -18,101 +18,104 @@ class _HomePageState extends State<HomePage> {
       ),
       body: _list(),
       floatingActionButton: new FloatingActionButton(
-        onPressed: () =>_addNote(), tooltip: 'Add task', child: new Icon(Icons.add)
-      ),
+          onPressed: () => _addNote(),
+          tooltip: 'Add task',
+          child: new Icon(Icons.add)),
     );
   }
 
-  Widget _list(){
+  Widget _list() {
     return ListView.builder(
       itemCount: notes.length,
       itemBuilder: (context, position) {
         var element = notes[position];
-        return _item(element, position);
+        return _item(element, position, element.title, element.body);
       },
     );
   }
 
-  Widget _item(Note element, int position){
+  Widget _item(Note element, int position, String title, String Body) {
     return Dismissible(
-      background: _backgroundSlide(),
-      key: UniqueKey(),
-      
-      confirmDismiss: (direction) async {
-        if (direction == DismissDirection.startToEnd) {
-          final bool res = await showDialog(
-              context: context,
-              builder: (BuildContext context) {
-                return AlertDialog(
-                  content: Text(
-                      "Are you sure you want to delete ${notes[position].title}?"),
-                  actions: <Widget>[
-                    FlatButton(
-                      child: Text(
-                        "Cancel",
-                        style: TextStyle(color: Colors.black),
+        background: _backgroundSlide(),
+        key: UniqueKey(),
+        confirmDismiss: (direction) async {
+          if (direction == DismissDirection.startToEnd) {
+            final bool res = await showDialog(
+                context: context,
+                builder: (BuildContext context) {
+                  return AlertDialog(
+                    content: Text(
+                        "Are you sure you want to delete ${notes[position].title}?"),
+                    actions: <Widget>[
+                      FlatButton(
+                        child: Text(
+                          "Cancel",
+                          style: TextStyle(color: Colors.black),
+                        ),
+                        onPressed: () {
+                          Navigator.of(context).pop();
+                        },
                       ),
-                      onPressed: () {
-                        Navigator.of(context).pop();
-                      },
-                    ),
-                    FlatButton(
-                      child: Text(
-                        "Add",
-                        style: TextStyle(color: Colors.red),
+                      FlatButton(
+                        child: Text(
+                          "Remove",
+                          style: TextStyle(color: Colors.red),
+                        ),
+                        onPressed: () {
+                          setState(() {
+                            notes.removeAt(position);
+                          });
+                          Navigator.of(context).pop();
+                        },
                       ),
-                      onPressed: () {
-                        setState(() {
-                          notes.removeAt(position);
-                        });
-                        Navigator.of(context).pop();
-                      },
-                    ),
-                  ],
-                );
-              });
-          return res;
-        } else {
-          // TODO: Navigate to edit page;
-        }
-      },      
-      
-      child: InkWell(
-          onTap: () {
-            print("${notes[position]} clicked");
-          },
-          child: ListTile(title: Text('${notes[position]}'))
-        ) // ListTile(title: Text('$element')),
-    );
+                    ],
+                  );
+                });
+            return res;
+          } else {
+            // TODO: Navigate to edit page;
+          }
+        },
+        child: Card(
+            color:
+                element.completed == 1 ? Colors.blueGrey : Colors.yellow[200],
+            child: InkWell(
+                onTap: () {
+                  print("${notes[position]} clicked");
+                },
+                child: ListTile(
+                  title: Text(title),
+                  subtitle: Text(Body),
+                )) // ListTile(title: Text('$element')),
+            ));
   }
 
-  Widget _backgroundSlide(){
+  Widget _backgroundSlide() {
     return Container(
-      color: Colors.red,
-      child: Align(
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.start,
-          children: <Widget>[
-            Icon(
-              Icons.delete,
-              color: Colors.white,
-            ),
-            Text(
-              " Delete",
-              style: TextStyle(
+        color: Colors.red,
+        child: Align(
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: <Widget>[
+              Icon(
+                Icons.delete,
                 color: Colors.white,
-                fontWeight: FontWeight.w700,
               ),
-              textAlign: TextAlign.left,
-            ),
-            SizedBox(
-              width: 20,
-            ),
-          ],
-        ),
-        alignment: Alignment.centerRight,
-      )
-    );
+              Text(
+                " Delete",
+                style: TextStyle(
+                  color: Colors.white,
+                  fontWeight: FontWeight.w700,
+                ),
+                textAlign: TextAlign.left,
+              ),
+              SizedBox(
+                width: 20,
+              ),
+            ],
+          ),
+          alignment: Alignment.centerRight,
+        ));
   }
 
   void _addNote() async {
@@ -126,7 +129,7 @@ class _HomePageState extends State<HomePage> {
     if (note != null) {
       setState(() {
         notes.add(note);
-      }); 
-    }  
+      });
+    }
   }
 }
